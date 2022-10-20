a798987c6b0680f1c17a33227ed4fc5df067af1e
Marin <linkempro29@gmail.com>
2022-10-20 23:19:22 +0200

    ajout sur branch initial

--- constants.py
+++ constants.py
@@ -1,6 +1,17 @@
+from extract_map import lieu
+
 X_SIZE = 50
 Y_SIZE = 50
 
+END_X = 13
+END_Y = 1
+
 PATH = "ressource\\p3.txt"
+
+list = [["ressource\\black.png", - 10, - 10, [13*X_SIZE, 1*Y_SIZE]],
+        ["ressource\\guardian.png", 0, 0, [13*X_SIZE, 1*Y_SIZE]],
+        ["ressource\\1.png", - 500, - 650, lieu[0]],
+        ["ressource\\2.png", - 175, - 175, lieu[1]],
+        ["ressource\\0.png", - 40, - 40, lieu[2]]]
 
 # set the constant and load the texte file
